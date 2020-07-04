75cf08766b82e1b7a0a4b82e286d67e8b52762f4
Vitor Henrique <vitor.henrique@esig.com.br>
2020-07-04 18:43:24 -0300

    Adicionado a estrutura da página Sobre

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Cabecalho from "./layout/Cabecalho";
 import MenuLateral from "./layout/MenuLateral";
 import Inicio from "./paginas/Inicio";
+import Sobre from "./paginas/Sobre";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
             <Cabecalho />
             <div className="conteudo p-3">
               <Switch>
-                <Route path="" exact={true} component={Inicio} />
+                <Route path="/" exact={true} component={Inicio} />
+                <Route path="/sobre" exact={true} component={Sobre} />
               </Switch>
             </div>
           </div>
---
+++ src/paginas/Sobre.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Sobre.css";
+
+export default function Sobre() {
+  return (
+    <div className="text-center mr-auto ml-auto w-95 bg-transparent rounded">
+      <h1 className="pt-4">Sobre</h1>
+    </div>
+  );
+}
