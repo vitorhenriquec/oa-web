407fc10d99b4c9b9a5a74d4e4e515523dda8e66f
Vitor Henrique <vitor.henrique@esig.com.br>
2020-07-11 21:02:19 -0300

    Adicionado botões de login rápido pelo Facebook e Google

--- src/paginas/login/Login.js
+++ src/paginas/login/Login.js
@@ -11,6 +11,26 @@ export default function Login() {
         <h6>Objeto de Aprendizagem</h6>
       </div>
       <form id="formLogin" className="m-auto">
+        <div className="row mt-2 mb-2">
+          <div className="col-sm-12 col-md-6">
+            <button
+              type="submit"
+              className="btn w-100 btn-secondary mb-1"
+              title="Login pelo Facebook"
+            >
+              <i className="fab fa-facebook-f"></i>Entrar
+            </button>
+          </div>
+          <div className="col-sm-12 col-md-6">
+            <button
+              type="submit"
+              className="btn w-100 btn-secondary mb-1"
+              title="Login pelo Google"
+            >
+              <i className="fab fa-google"></i>Entrar
+            </button>
+          </div>
+        </div>
         <div className="form-group">
           <input
             type="email"
@@ -35,10 +55,18 @@ export default function Login() {
             onClick={(event) => setVisibilidadeSenha(!visibilidadeSenha)}
           ></i>
         </div>
-        <button type="submit" className="btn w-100 btn-primary mb-1">
+        <button
+          type="submit"
+          className="btn w-100 btn-primary mb-1"
+          title="Entrar"
+        >
           <i className="fa fa-sign-in-alt fa-lg mr-1"></i>Entrar
         </button>
-        <button type="submit" className="btn w-100 btn-dark">
+        <button
+          type="submit"
+          className="btn w-100 btn-dark"
+          title="Cadastrar-se"
+        >
           <i className="fa fa-user-plus fa-lg mr-1"></i>Cadastre-se
         </button>
       </form>
